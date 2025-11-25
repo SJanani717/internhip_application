9b9e01db1e880df7d6330eb93ff7b7acdb816713
Janani sathiyasuthan <sjanani712@gmail.com>
2025-11-25 21:18:38 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // Default XAMPP password is empty
+$dbname = "intern_applications";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
